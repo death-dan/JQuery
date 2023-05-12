4ff86490c3cdad6ced196bc150b07ae64c5cdcbe
Fernando <death_dan@hotmail.com>
2023-05-12 11:34:32 -0300

    Propriedade [Dimensão]

---
+++ 03-manipuladores/06-dimensao.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Classes</title>
+
+    <style>
+        body {
+            padding: 10%;
+        }
+
+        div {
+            background-color: #eee;
+            padding: 15px;
+            border-bottom: 1px solid #ccc;
+        }
+
+        div p {
+            font-size: 0.8em;
+            margin: 10px 0 0 0;
+        }
+
+    </style>
+</head>
+<body>
+
+    <div style="border: 5px solid #ccc;">
+        <span>Hey, estamos aprendendo <b>jQuery</b> aqui !</span>
+        <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+            Placeat, unde incidunt sunt delectus asperiores autem eius earum 
+            cumque dignissimos perferend.
+        </p>
+    </div>
+    <div>
+        <span>E com isso podemos criar interfaces incríveis</span>
+        <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+            Placeat, unde incidunt sunt delectus asperiores autem eius earum 
+            cumque dignissimos perferend.
+        </p>
+    </div>
+    <div>
+        <span>Então #BoraPrograma !!</span>
+        <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+            Placeat, unde incidunt sunt delectus asperiores autem eius earum 
+            cumque dignissimos perferend.
+        </p>
+    </div>
+    <div>
+        <span>jQuery Essentials !!</span>
+        <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+            Placeat, unde incidunt sunt delectus asperiores autem eius earum 
+            cumque dignissimos perferend.
+        </p>
+    </div>
+
+    <script src="../js/jquery.js"></script>
+    <script src="dimensao.js"></script>
+</body>
+</html>
