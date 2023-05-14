e6d2d26c4d00473ff429f9314777f54e67598f5f
Fernando <death_dan@hotmail.com>
2023-05-14 12:26:59 -0300

    Filtragem [Condição]

---
+++ 04-atravessadores/02-condicao.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Filtragem [Posição]</title>
+
+    <style>
+        body {
+            padding: 10%;
+        }
+
+        div {
+            padding: 10px;
+            background: #000;
+            color: #fff;
+            margin-top: 20px;
+        }
+    </style>
+</head>
+<body>
+    <p>Artigo HTML</p>
+    <p class="j">Artigo PHP</p>
+    <p>Artigo <b>jQuery</b></p>
+    <p>Artigo <span>MariaDB</span></p>
+
+    <script src="../js/jquery.js"></script>
+    <script src="condicao.js"></script>
+</body>
+</html>
