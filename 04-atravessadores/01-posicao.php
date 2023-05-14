528d0fb77c8b494bd7486d1582efeb3c4cdb3b0c
Fernando <death_dan@hotmail.com>
2023-05-13 22:41:47 -0300

    Filtragem [Posição]

---
+++ 04-atravessadores/01-posicao.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Filtragem [Posição]</title>
+
+    <style>
+        body {
+            padding: 10%;
+        }
+
+    </style>
+</head>
+<body>
+    <p>Artigo</p>
+    <p>Artigo</p>
+    <p>Artigo</p>
+    <p>Artigo</p>
+
+    <script src="../js/jquery.js"></script>
+    <script src="posicao.js"></script>
+</body>
+</html>
