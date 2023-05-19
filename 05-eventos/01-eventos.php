907f9d44f3b7b98b1820ba33c55fa6dab6b95839
Fernando <death_dan@hotmail.com>
2023-05-19 13:22:17 -0300

    Criando 05 Eventos

---
+++ 05-eventos/01-eventos.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Eventos</title>
+
+    <style>
+        body {
+            padding: 10%;
+        }
+    </style>
+</head>
+<body>
+    <article class="article">
+        <header class="article_header">
+            <h1>Curso jQuery Essentials</h1>
+            <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+                Libero esse quidem commodi repellat numquam similique maiores 
+                expedita deserunt fugiat perspiciatis a nihil placeat, dolores 
+                eligendi optio, aliquid veritatis explicabo iure.
+            </p>
+        </header>
+
+        <div class="article_content">
+            <p>Lorem Ipsum is simply dummy text of the <mark>printing and typesetting industry.</mark> Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+            <p>It has survived not only five centuries, but also the leap <a href="#" title="Link">into electronic</a> typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing...</p>
+            <p>Lorem Ipsum passages, <b>and more recently with desktop <a href="#" title="Link">into electronic</a> publishing software like Aldus PageMaker</b> including versions of Lorem Ipsum.</p>
+        </div>
+    </article>
+
+    <script src="../js/jquery.js"></script>
+    <script src="eventos.js"></script>
+</body>
+</html>
