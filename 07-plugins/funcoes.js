5a167f3d55e0b34dae2a1b07b812ed15d231e346
Fernando <death_dan@hotmail.com>
2023-05-24 15:49:57 -0300

    Criando Funções

--- 07-plugins/funcoes.js
+++ 07-plugins/funcoes.js
@@ -1,3 +1,31 @@
 $(function () {
+    //normalizeH("div div");
+    //normalizeH("div .p");
+    normalizeH(".normalize");
+    $(window).resize(function () {
+        normalizeH(".normalize");
+    });
 
+    function normalizeH(element) {
+        var maxHeight = {};
+        $.each($(element).parent(), function (i, e) {
+            $(e).find(element).css("height", "auto");
+            maxHeight = $(e).find(element).map(function (il, el) {
+                return $(el).height();
+            }).get();
+
+            $(e).find(element).height(Math.max.apply(this, maxHeight));
+            console.log(maxHeight);
+        });
+    }
+
+    function normalizeH_back(element) {
+        $(e).find(element).css("height", "auto");
+        var maxHeight = $(element).map(function (i, e) {
+            return $(e).height();
+        }).get();
+
+        $(element).height(Math.max.apply(this, maxHeight));
+        console.log(maxHeight);
+    }
 });
